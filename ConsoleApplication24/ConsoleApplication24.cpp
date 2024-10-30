824fcc15e1c7faa301dacceb2e3f3645e66a66a7
Егор <@DESKTOP-VF4U8Q2>
2024-10-30 18:06:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication24/ConsoleApplication24.cpp
@@ -0,0 +1,35 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+   
+    int kilometer, time;
+   
+    cout << "Привет, Сэм! Сколько километров ты сегодня пробежал? ";
+    cin >> kilometer;
+    
+    int totalTime = 0;
+    int i = 0;
+    int circle = 1;
+   
+
+    while (i < kilometer) {
+        int time;
+        cout << "Какой у тебя был темп на километре " << circle << "? ";
+        cin >> time;
+        totalTime += time;
+        i++;
+        circle++;
+        
+    }
+    
+    int resultTime = round(totalTime / kilometer);
+
+    int minutes = resultTime / 60;
+    int seconds = resultTime % 60;
+
+    cout << "Твой средний темп за тренировку: " << minutes << " минуты " << seconds << " секунд.";
+}
+
